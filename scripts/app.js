f829d89901ac3d275239123f184f46951974707e
nicholasgacicia <nicholasgacicia@gmail.com>
2020-09-10 19:21:37 -0500

    Commit 2 - Get Even answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,15 @@
 console.log('Weekapaug');
 
 // Easy Going
-// 1)
+// 1) Write a for loop that will log the numbers 1 through 20.
 for (let i = i; i < 21; i++) {
    console.log(i);
 }
 
+// Get Even
+// 1) Write a for loop that will log only the even numbers in 0 through 200.
+const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+for (let i = 1; i <= numbersList.length - 1; i++) {
+
+}
+console.log(numbersList);
