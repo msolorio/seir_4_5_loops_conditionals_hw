b7246c2a9df5272b25ae772c6f0ab3081aa22fa5
nicholasgacicia <nicholasgacicia@gmail.com>
2020-09-17 15:26:13 -0500

    Commit 1 - Easy Going answered

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,6 @@
-console.log('Weekapaug');
-
-// ----- Easy Going
+// -----  Easy Going
 // 1) Write a for loop that will log the numbers 1 through 20.
-for (let i = i; i < 21; i++) {
+for (let i = 1; i <= 20; i++) {
    console.log(i);
 }
 
