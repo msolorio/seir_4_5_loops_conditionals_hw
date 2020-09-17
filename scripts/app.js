998a78242585890a7f6eeee8776a98420492fc20
nicholasgacicia <nicholasgacicia@gmail.com>
2020-09-17 15:05:24 -0500

    Copied questions into app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,15 +1,160 @@
 console.log('Weekapaug');
 
-// Easy Going
+// ----- Easy Going
 // 1) Write a for loop that will log the numbers 1 through 20.
 for (let i = i; i < 21; i++) {
    console.log(i);
 }
 
-// Get Even
+// ----- Get Even
 // 1) Write a for loop that will log only the even numbers in 0 through 200.
 const numbersList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 for (let i = 1; i <= numbersList.length - 1; i++) {
 
 }
 console.log(numbersList);
+
+// ----- Excited Kitten
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// 2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+
+// ----- Fizz Buzz
+// Write a javascript application that logs all numbers from 1 - 100 AND:
+//   1. If a number is divisible by 3 log "Fizz" instead of the number.
+//   2. If a number is divisible by 5 log "Buzz" instead of the number.
+//   3. If a number is divisible by 3 and 5 log "FizzBuzz" instead of the number, "Fizz", or "Buzz".
+
+// ----- Getting to Know You
+// Use the following arrays to answer the questions below(name, age, hometown):
+/*
+const kenny = ["Kenny", 1000, "Austin"];
+const jimHaff = ["Jim H", 16, "All cities"];
+const reuben = ["Reuben", 22, "Durham"];
+const jimClark = ["Jim C", 186, "LA"];
+const ryan = ["Ryan", 65, "Denver"];
+*/
+// 1. Jim Clark decides that Kenny can't be named "Kenny" anymore. Remove "Kenny" from the kenny array and replace it with "Gameboy".
+// 2. Jim Clark just had his birthday; change jimClark's array to reflect him being a year older. Don't just hard code 187--pretend that you didn't already know that his age is 186, and write your code to just make him a year older than whatever age he was.
+// 3. Ryan is Batman maybe.Or possibly Robin.Change Ryan's hometown from "Denver" to "Gotham City".
+// 4. Reuben left Durham 5 years ago to come to Chicago.First, remove "Durham" from Reuben's array, and then add "Chicago". (Note: remove and then add is different from simply changing the value at that index.)
+// 5. Jim Haff could be literally anywhere in the world.Remove "All cities" from his array, then pick any 3 cities you like, and add them to Jim's array. If you did it in 3 lines of code that's fine, but see if you can do it in one line of code
+// 6. Bonus: Whoops! Jim Haff is actually only allowed to be in one of two cities.Whatever the first of the 3 cities for Jim Haff is now, remove it from the array using.splice()
+
+// ----- Yell at the Ninja Turtles
+// 1. Create an array with the members of the ninja turtles(Donatello, Leonardo, Raphael, Michaelangelo)
+// 2. Use a for loop to call.toUpperCase() on each of them and print out the result.
+// 3. Bonus: Modify the answer you just wrote.Instead of all letters being uppercase, make the letters alternate back and forth between uppercase and lowercase.
+
+// ----- Return of the Closets
+// Below, we've given you examples of Kristyn and Thom's closets modeled as data in JavaScript.Use this data to answer the following questions.
+/*
+const kristynsCloset = [
+   "left shoe",
+   "cowboy boots",
+   "right sock",
+   "GA hoodie",
+   "green pants",
+   "yellow knit hat",
+   "marshmallow peeps"
+];
+
+// Thom's closet is more complicated. Check out this nested data structure!!
+const thomsCloset = [
+   [
+      // These are Thom's shirts
+      "grey button-up",
+      "dark grey button-up",
+      "light blue button-up",
+      "blue button-up",
+   ], [
+      // These are Thom's pants
+      "grey jeans",
+      "jeans",
+      "PJs"
+   ], [
+      // Thom's accessories
+      "wool mittens",
+      "wool scarf",
+      "raybans"
+   ]
+];
+*/
+// --- Alien Attire
+// 1. Kristyn's left shoe has traveled through time and space and turned up in Thom's accessories drawer! Remove Kristyn's shoe from the array and save it to the variable kristynsShoe. Use that variable to add Kristyn's lost shoe to Thom's accessories array.
+// --- Dress 'em Up
+// 1. Modify your code to put together 3 separate outfits for Kristyn and Thom.Put the output in a sentence to tell us what we'll be wearing. Mix and match!
+
+// ----- Dirty Laundry
+// Continue looking at the closet arrays:
+// 1. Time to do laundry - loop through Kristyn's closet and log the sentence "WHIRR: Now washing (item)" for each item in the array.
+// ----- Inventory
+// 2. Thom wants to do inventory on his closet.Using bracket notation, log the arrays(not the elements in the arrays) containing all of Thom's shirts, pants, and accessories.
+
+// ----- Multiples of 3 and 5
+// Yes, you might have tackled this earlier, but try it again(don't look back at your other code 👀)
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+// ----- Hungry for more ?
+//   Triangles
+// 0. declare a variable argument and set it equal to 7.
+// 1. Write a loop that console logs a "left isosceles" triangle(SEE BELOW) made of '#' that has the height and length of argument.
+/*
+   Ex: argument is 7
+#
+##
+###
+####
+#####
+######
+#######
+*/
+// 2. Write a loop that console logs a "right isosceles" triangle(SEE BELOW) made of '#' that has the height and length of argument.This is deceptively tricky.
+/*
+   Ex: argument is 7
+
+      #
+     ##
+    ###
+   ####
+  #####
+ ######
+#######
+*/
+// 3. Write a loop that console logs an "upside down left" isosceles triangle made of '#' that has the height and length of the argument.
+/*
+   Ex: argument is 7
+
+#######
+######
+#####
+####
+###
+##
+#
+*/
+// 4. Write a loop that console logs an "upside down right" isosceles triangle made of '#' that has the height and length of the argument.This is also tricky.
+/*
+   Ex: argument is 7
+
+#######
+ ######
+  #####
+   ####
+    ###
+     ##
+      #
+*/
+// 5. Change the value of argument and reload your code and marvel at how you just solved a challenging problem and feel proud of yourself.
+
+// ----- Find the Median
+// -Find the median number in the following nums array, then console.log that number.
+//hint this will likely involve breaking down the problem into a few steps
+//hint: Click "Details" below(don't read this unless you've been stuck for a while)
+//   Details: if you check the length of the array / 2, you might get not get a whole number.In which case, look into`Math.floor( // something )`
+/*
+const nums = [14, 11, 16, 15, 13, 16, 15, 17, 19, 11, 12, 14, 19, 11, 15, 17, 11, 18, 12, 17, 12, 71, 18, 15, 12];
+
+Expected output:
+=> 15
+*/
