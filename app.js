fd61f41ad8a0ce9bf66321b8164e7896bb25d74e
msolorio <msolorio.github@gmail.com>
2021-04-10 23:22:49 -0700

    Adds solution for get even and excited kitten

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+// EASY GOING
 // 1. Write a for loop that will log the numbers 1 through 20.
-for (let i = 1; i <= 20; i++) {
-  console.log(i);
-}
+// for (let i = 1; i <= 20; i++) {
+//   console.log(i);
+// }
 
+// GET EVEN
+// 1. Write a for loop that will log only the even numbers in 0 through 200.
+// for (let i = 0; i <= 200; i += 2) {
+//   console.log(i);
+// }
+
+// EXCITED KITTEN
+// 1. Write code that logs "Love me, pet me! HSSSSSS!" 20 times.
+// 2. For every even number in your loop, log "...human...why you taking pictures of me?...", "...the catnip made me do it...", or "...why does the red dot always get away..." at random.
+// const catResponses = [
+//   '...human...why you taking pictures of me?...',
+//   '...the catnip made me do it...',
+//   '...why does the red dot always get away...'
+// ];
+
+// for (let i = 0; i < 20; i++) {
+//   console.log('Love me, pet me! Hsssssss');
+
+//   if (i % 2 === 0) {
+//     const randomIndex = Math.floor(Math.random() * catResponses.length);
+
+//     console.log(catResponses[randomIndex]);
+//   }
+// }
